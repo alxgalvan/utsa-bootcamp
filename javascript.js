ba818850e777424549beab06f1ac366e7a1c373e
Alex <61997301+alxgalvan@users.noreply.github.com>
2020-04-10 12:50:07 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("box").style.backgroundColor = "blue";
+    document.getElementById("box").style.height = "150px";
+});
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+document.getElementById("button4").addEventListener("click", function() {
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+document.getElementById("button3").addEventListener("click", function () {
+    document.getElementById("box").style.backgroundColor = "lightorange";
+});
